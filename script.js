89b8f51839b1cdb627bf3c8d34fbb02a4b77e0c2
Jesper <jesper_muff@hotmail.com>
2022-01-29 13:53:39 +0000

    compute function ok

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    final_value = p * (1 + rate) ** years
+
+    cur_year = new Date().getFullYear()
+    final_year = cur_year + years 
+
+    document.getElementById("text").innerHTML = 
+    "If you deposit" + p + "at an interest of " + rate + "%. You will receive an amount of " + final_value + ","+"in the year" + final_year + "."
     
 }
         
